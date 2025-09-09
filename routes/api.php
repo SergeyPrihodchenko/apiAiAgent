2fd8e8a73dbb1650aba088f6a8e26700a9be61be
Your Name <you@example.com>
2025-09-09 23:35:24 +0300

    a

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,5 @@
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'api'], function() {
-    Route::post('/test', [TestController::class, 'index']);
+Route::group([], function() {
 });
